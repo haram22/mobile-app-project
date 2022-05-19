ac73832acf032150348f801c3ab348f4e2971586
haram <kimhr9889@handong.ac.kr>
2022-05-19 19:16:11 +0900

    firebase login

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,5 @@ import 'package:firebase_core/firebase_core.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  //await firebase_core.Firebase.initializeApp();
   runApp(LoginPage());
 }
-// Future<void> main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await Firebase.initializeApp();
-//   //await firebase_core.Firebase.initializeApp();
-//   runApp(Login());
-// }
