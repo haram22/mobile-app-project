38d7320cf2a5e58d0c2109bc68dee66d4b8360b7
haram <kimhr9889@handong.ac.kr>
2022-05-19 19:11:54 +0900

    login firebase

---
+++ lib/home.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Image.network('https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/HGU-Emblem-eng.svg/1024px-HGU-Emblem-eng.svg.png?20200507143923', height: 57,),
+        actions: [
+          IconButton(onPressed: (){}, icon: Icon(Icons.search))
+        ],
+      ),
+    );
+  }
+}
